6341e9ec0717cffaea363ee86fe733c50889b7a6
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-09-04 14:59:20 +0530

    first Commit

---
+++ Assignment17/Assignment17/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment17.Models;
+
+namespace Assignment17.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment17.Models.Movie> Movie { get; set; } = default!;
+    }
+}
